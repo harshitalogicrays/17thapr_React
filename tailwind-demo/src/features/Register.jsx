8bbc198391373e9e747a6aa36818a1a98f2c7d63
harshita <harshita@logicrays.com>
2024-09-13 16:16:59 +0530

    13sep

--- tailwind-demo/src/features/Register.jsx
+++ tailwind-demo/src/features/Register.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { ADD_USER } from '../redux/userSlice'
 
 const Register = () => {
+  const dispatch = useDispatch()
+  const [user,setUser]=useState({username:'',email:'',password:'',cpassword:''})
+  let handleSubmit=(e)=>{
+    e.preventDefault()
+    dispatch(ADD_USER(user))
+  }
   return (
     <>
        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -10,13 +18,13 @@ const Register = () => {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Username
               </label>
               <div className="mt-2">
-                <input   name="username" type="text" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                <input   name="username" type="text" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})}
                 />
               </div>
             </div>
@@ -26,6 +34,7 @@ const Register = () => {
               </label>
               <div className="mt-2">
                 <input   name="email" type="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})}
                 />
               </div>
             </div>
@@ -38,6 +47,7 @@ const Register = () => {
               <div className="mt-2">
                 <input name="password" type="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})}
                 />
               </div>
             </div>
@@ -50,6 +60,7 @@ const Register = () => {
               <div className="mt-2">
                 <input name="cpassword" type="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={user.cpassword} onChange={(e)=>setUser({...user,cpassword:e.target.value})}
                 />
               </div>
             </div>
@@ -58,7 +69,7 @@ const Register = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-3 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
--- tailwind-demo/src/redux/userSlice.js
+++ tailwind-demo/src/redux/userSlice.js
@@ -2,10 +2,17 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const userSlice = createSlice({
 name:"user",
-initialState:{},
-reducers:{
-
+initialState:{users:[]},
+reducers:{//pure functions
+    ADD_USER:(state,action)=>{
+        console.log("add user",action.payload)
+        state.users.push(action.payload)
+    },
+    REMOVE_USER(state,action){}
 }
 })
+export const {ADD_USER,REMOVE_USER} = userSlice.actions
+
+// console.log(userSlice.actions)
 
 export default userSlice
